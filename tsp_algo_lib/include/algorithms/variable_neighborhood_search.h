5be7951f1ab9985c456c894b14b729e0167576b3
Biblink02 <alby.bot.24@gmail.com>
2025-11-23 09:57:42 +0100

    refactor: variable_neighborhood_search adapted to new architecture

--- tsp_algo_lib/include/algorithms/variable_neighborhood_search.h
+++ tsp_algo_lib/include/algorithms/variable_neighborhood_search.h
@@ -1,24 +1,17 @@
 #ifndef VARIABLE_NEIGHBORHOOD_SEARCH_H
 #define VARIABLE_NEIGHBORHOOD_SEARCH_H
+
 #include "tsp_algorithm.h"
 
-/**
- * @brief Configuration for the Variable Neighborhood Search algorithm.
- */
 typedef struct {
-    const int kick_repetition; /**< Number of kick repetitions. */
-    const double time_limit; /**< Maximum allowed time for the search. */
-    const int n_opt;
-} VariableNeighborhoodSearch;
+    int kick_repetition;
+    int n_opt;
+    double time_limit;
+} VNSConfig;
 
 /**
- * @brief Initializes a TSP algorithm using the Variable Neighborhood Search (VNS) approach.
- *
- * @param kick_repetition Number of perturbations applied in the shaking phase.
- * @param time_limit Maximum allowed execution time in seconds.
- * @return Pointer to a TspAlgorithm instance configured for VNS.
+ * @brief Creates a VNS algorithm strategy.
  */
-const TspAlgorithm *init_vns(int kick_repetition, int n_opt, double time_limit);
+TspAlgorithm vns_create(VNSConfig config);
 
-
-#endif //VARIABLE_NEIGHBORHOOD_SEARCH_H
+#endif // VARIABLE_NEIGHBORHOOD_SEARCH_H
--- tsp_algo_lib/src/algorithm/variable_neighborhood_search.c
+++ tsp_algo_lib/src/algorithm/variable_neighborhood_search.c
@@ -1,146 +1,92 @@
 #include "variable_neighborhood_search.h"
-#include <string.h>
-#include <stdlib.h>
-#include <c_util.h>
 #include "algorithms.h"
-#include "constants.h"
-#include "costs_plotter.h"
-#include "time_limiter.h"
-#include "tsp_instance.h"
-#include "tsp_solution.h"
 #include "tsp_math_util.h"
+#include "time_limiter.h"
+#include "c_util.h"
 #include "logger.h"
+#include <stdlib.h>
+#include <string.h>
 
+#include "constants.h"
 
-union TspExtendedAlgorithms {
-    VariableNeighborhoodSearch *variable_neighborhood_search;
-};
+static double vns_kick(int *tour, int n, const double *costs, int n_opt) {
+    int *edges = malloc(n_opt * sizeof(int));
+    check_alloc(edges);
 
-static double kick(int tour[],
-                   const int number_of_nodes,
-                   const double *edge_cost_array,
-                   const int n_opt) {
-    int edges_to_remove[n_opt];
+    rand_k_non_contiguous(0, n - 1, n_opt, edges);
 
-    rand_k_non_contiguous(0, number_of_nodes - 1, n_opt, edges_to_remove);
+    const double delta = compute_n_opt_cost(n_opt, tour, edges, costs, n);
+    compute_n_opt_move(n_opt, tour, edges, n);
 
-    const double delta = compute_n_opt_cost(n_opt, tour, edges_to_remove, edge_cost_array, number_of_nodes);
-
-    compute_n_opt_move(n_opt, tour, edges_to_remove, number_of_nodes);
-
+    free(edges);
     return delta;
 }
 
-static void improve(const TspAlgorithm *tsp_algorithm,
-                    const TspInstance *instance,
+static void run_vns(const TspInstance *instance,
                     TspSolution *solution,
-                    CostsPlotter *plotter) {
-    if_verbose(VERBOSE_DEBUG, "  VNS: Starting improvement loop...\n");
-    const int number_of_nodes = tsp_instance_get_num_nodes(instance);
-    const double *edge_cost_array = tsp_instance_get_cost_matrix(instance);
+                    const void *config_void,
+                    CostRecorder *recorder) {
+    const VNSConfig *cfg = config_void;
+    const int n = tsp_instance_get_num_nodes(instance);
+    const double *costs = tsp_instance_get_cost_matrix(instance);
 
-    const double time_limit = tsp_algorithm->extended->variable_neighborhood_search->time_limit;
-    const int kick_repetition = tsp_algorithm->extended->variable_neighborhood_search->kick_repetition;
-    const int n_opt = tsp_algorithm->extended->variable_neighborhood_search->n_opt;
+    if_verbose(VERBOSE_DEBUG, "  VNS (k=%d, n_opt=%d, t=%.2f)\n",
+               cfg->kick_repetition, cfg->n_opt, cfg->time_limit);
 
-    if_verbose(VERBOSE_DEBUG, "  VNS: Time limit=%.2fs, Kicks=%d, N-Opt=%d\n",
-               time_limit, kick_repetition, n_opt);
+    TimeLimiter timer = time_limiter_create(cfg->time_limit);
+    time_limiter_start(&timer);
 
-    TimeLimiter *time_limiter = time_limiter_create(time_limit);
-    time_limiter_start(time_limiter);
+    int *current_tour = malloc((n + 1) * sizeof(int));
+    check_alloc(current_tour);
 
-    int current_tour[number_of_nodes + 1];
     tsp_solution_get_tour(solution, current_tour);
     double current_cost = tsp_solution_get_cost(solution);
 
-    if_verbose(VERBOSE_DEBUG, "  VNS: Running initial 2-Opt...\n");
-    current_cost += two_opt(current_tour, number_of_nodes, edge_cost_array, time_limiter, EPSILON);
+    current_cost += two_opt(current_tour, n, costs, timer, );
 
-    int best_tour[number_of_nodes + 1];
-    memcpy(best_tour, current_tour, (number_of_nodes + 1) * sizeof(int));
+    int *best_tour = malloc((n + 1) * sizeof(int));
+    check_alloc(best_tour);
+    memcpy(best_tour, current_tour, (n + 1) * sizeof(int));
     double best_cost = current_cost;
-    if_verbose(VERBOSE_DEBUG, "  VNS: Initial cost after 2-Opt: %lf\n", best_cost);
 
-    while (!time_limiter_is_over(time_limiter)) {
-        // Kicking
-        for (int i = 0; i < kick_repetition; i++) {
-            current_cost += kick(current_tour, number_of_nodes, edge_cost_array, n_opt);
+    while (!time_limiter_is_over(&timer)) {
+        for (int i = 0; i < cfg->kick_repetition; i++) {
+            current_cost += vns_kick(current_tour, n, costs, cfg->n_opt);
         }
 
-        // Local Search (2-Opt)
-        current_cost += two_opt(current_tour, number_of_nodes, edge_cost_array, time_limiter, EPSILON);
+        current_cost += two_opt(current_tour, n, costs, timer, );
 
         if (current_cost < best_cost - EPSILON) {
-            if_verbose(VERBOSE_DEBUG, "    VNS: Found new best cost: %lf\n", current_cost);
-            memcpy(best_tour, current_tour, (number_of_nodes + 1) * sizeof(int));
+            if_verbose(VERBOSE_DEBUG, "    VNS: New best cost: %lf\n", current_cost);
             best_cost = current_cost;
+            memcpy(best_tour, current_tour, (n + 1) * sizeof(int));
+        } else {
+            memcpy(current_tour, best_tour, (n + 1) * sizeof(int));
+            current_cost = best_cost;
         }
-        costs_plotter_add(plotter, current_cost);
-    }
 
-    if (time_limiter_is_over(time_limiter)) {
-        if_verbose(VERBOSE_DEBUG, "  VNS: Improvement loop stopped due to time limit.\n");
+        cost_recorder_add(recorder, current_cost);
     }
 
     tsp_solution_update_if_better(solution, best_tour, best_cost);
 
-    if_verbose(VERBOSE_DEBUG, "  VNS: Cleaning up time limiter.\n");
-    time_limiter_destroy(time_limiter);
+    free(best_tour);
+    free(current_tour);
 }
 
-static void solve(const TspAlgorithm *tsp_algorithm,
-                  const TspInstance *instance,
-                  TspSolution *solution,
-                  CostsPlotter *plotter) {
-    if_verbose(VERBOSE_INFO, "Running Variable Neighborhood Search algorithm...\n");
-    const int number_of_nodes = tsp_instance_get_num_nodes(instance);
-    const double *edge_cost_array = tsp_instance_get_cost_matrix(instance);
-
-    int initial_tour[number_of_nodes + 1];
-    double initial_cost;
-    tsp_solution_get_tour(solution, initial_tour);
-
-    if_verbose(VERBOSE_DEBUG, "  VNS: Generating initial solution via Nearest Neighbor...\n");
-    const int result = nearest_neighbor_tour(rand() % number_of_nodes, initial_tour, number_of_nodes, edge_cost_array,
-                                             &initial_cost);
-
-    if (result != 0) {
-        if_verbose(VERBOSE_INFO, "  VNS: Failed to generate initial solution.\n");
-        return; // Exit if NN tour failed
-    }
-
-    tsp_solution_update_if_better(solution, initial_tour, initial_cost);
-    if_verbose(VERBOSE_DEBUG, "  VNS: Initial solution cost: %lf\n", initial_cost);
-
-    improve(tsp_algorithm, instance, solution, plotter);
-    if_verbose(VERBOSE_INFO, "Variable Neighborhood Search algorithm finished.\n");
+static void free_vns_config(void *config) {
+    free(config);
 }
 
+TspAlgorithm vns_create(const VNSConfig config) {
+    VNSConfig *cfg_copy = malloc(sizeof(VNSConfig));
+    check_alloc(cfg_copy);
+    *cfg_copy = config;
 
-static void free_this(const TspAlgorithm *self) {
-    if (!self) return;
-    if_verbose(VERBOSE_DEBUG, "Freeing VNS algorithm struct...\n");
-    if (self->extended) {
-        if (self->extended->variable_neighborhood_search) free(self->extended->variable_neighborhood_search);
-        free(self->extended);
-    }
-    free((void *) self);
-}
-
-const TspAlgorithm *init_vns(const int kick_repetition, const int n_opt, const double time_limit) {
-    if_verbose(VERBOSE_DEBUG, "Initializing VNS (k=%d, n_opt=%d, t=%.2f)\n", kick_repetition, n_opt, time_limit);
-    const VariableNeighborhoodSearch vns = {
-        .kick_repetition = kick_repetition,
-        .time_limit = time_limit,
-        .n_opt = n_opt,
+    return (TspAlgorithm){
+        .name = "VNS",
+        .config = cfg_copy,
+        .run = run_vns,
+        .free_config = free_vns_config
     };
-    const TspExtendedAlgorithms extended_algorithms = {
-        .variable_neighborhood_search = memdup(&vns, sizeof(vns))
-    };
-    const TspAlgorithm tsp_algorithm = {
-        .solve = solve,
-        .free = free_this,
-        .extended = memdup(&extended_algorithms, sizeof(extended_algorithms)),
-    };
-    return memdup(&tsp_algorithm, sizeof(tsp_algorithm));
 }
