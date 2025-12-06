bfba6c87873cc2a21de2d6e0f3ed9faa1d440d86
Biblink02 <118203931+Biblink02@users.noreply.github.com>
2025-12-06 18:41:22 +0100

    feat: use of random in tests

--- tsp_algo_lib/tests/src/grasp_nn_helpers_test.c
+++ tsp_algo_lib/tests/src/grasp_nn_helpers_test.c
@@ -1,7 +1,9 @@
 #include "grasp_nn_helpers_test.h"
 #include <assert.h>
+#include <limits.h>
 #include <stdio.h>
 #include "constructive.h"
+RandomState rng;
 
 static void create_dummy_costs(double *costs, int n) {
     for (int i = 0; i < n; i++) {
@@ -21,7 +23,7 @@ static void test_grasp_construction_basic(void) {
 
     // Run with defined RCL parameters
     // RCL Size = 3, Probability = 0.5
-    int res = grasp_nearest_neighbor_tour(0, tour, n, costs, &cost, 3, 0.5);
+    int res = grasp_nearest_neighbor_tour(0, tour, n, costs, &cost, 3, 0.5, &rng);
 
     assert(res == 0);
     assert(tour[0] == 0);
@@ -38,12 +40,13 @@ static void test_grasp_invalid_start_node(void) {
 
     // Start node 10 is out of bounds [0, 5)
     // RCL params don't matter here as it should fail early
-    int res = grasp_nearest_neighbor_tour(10, tour, n, costs, &cost, 3, 0.5);
+    int res = grasp_nearest_neighbor_tour(10, tour, n, costs, &cost, 3, 0.5, &rng);
     assert(res == -1);
 }
 
 void run_grasp_nn_helpers_tests(void) {
     printf("[GRASP Helper] Running tests...\n");
+    random_init(&rng, global_random_int(0,INT_MAX));
     test_grasp_construction_basic();
     test_grasp_invalid_start_node();
     printf("[GRASP Helper] Passed.\n");
