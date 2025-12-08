c79290e096425b67eb8d60d13f9436214ab21afc
Biblink02 <alby.bot.24@gmail.com>
2025-12-08 15:58:59 +0100

    refactor: small changes

--- common/include/c_util.h
+++ common/include/c_util.h
@@ -6,10 +6,12 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "random.h"
+
 /* public utility functions implemented in c_util.c */
 void check_alloc(const void *ptr);
 
-void check_popen(FILE * gp);
+void check_popen(FILE *gp);
 
 void check_pclose(int status);
 
@@ -19,12 +21,6 @@ void join_path(char *out, const char *dir, const char *file, size_t maxlen);
 
 void str_trim(char *s);
 
-/* inline utility: stays in header */
-static inline const char *str_file_ext(const char *filename) {
-    const char *dot = strrchr(filename, '.');
-    if (!dot || dot == filename) return NULL;
-    return dot + 1;
-}
 
 /* array equality macros */
 #define DEFINE_ARRAYS_EQUAL(type, suffix, check)                        \
@@ -48,6 +44,7 @@ static inline void swap_int(int *a, int *b) {
     *a = *b;
     *b = tmp;
 }
+
 /* swap macros */
 #define DEFINE_SWAP(type, suffix)                                   \
 static inline void swap_array_##suffix(type arr[], size_t i, size_t j) {  \
@@ -77,53 +74,17 @@ DEFINE_REVERSE_ARRAY(int, int)
 DEFINE_REVERSE_ARRAY(double, double)
 DEFINE_REVERSE_ARRAY(float, float)
 
-/* copy array macros */
-#define DEFINE_COPY_ARRAY(type, suffix)                                                       \
-static inline void copy_##suffix##_array(const type src[], type dest[], size_t count) {       \
-    for (size_t i = 0; i < count; i++) dest[i] = src[i];                                       \
-}
-
-DEFINE_COPY_ARRAY(int, int)
-DEFINE_COPY_ARRAY(double, double)
-DEFINE_COPY_ARRAY(float, float)
-
 /* shuffle macros */
-#define DEFINE_SHUFFLE_ARRAY(type, suffix)                     \
-static inline void shuffle_##suffix##_array(type arr[], size_t n) {  \
-    if(n < 2) return;                                          \
-    for (size_t i = n - 1; i > 0; i--) {                       \
-        size_t j = rand() % (i + 1);                           \
-        type tmp = arr[i];                                     \
-        arr[i] = arr[j];                                       \
-        arr[j] = tmp;                                          \
-    }                                                          \
+#define DEFINE_SHUFFLE_ARRAY(type, suffix)                                              \
+static inline void shuffle_##suffix##_array(type arr[], size_t n, RandomState *rng) {   \
+    if(n < 2) return;                                                                   \
+    for (size_t i = n - 1; i > 0; i--) {                                                \
+        size_t j = random_int(rng, 0, i);                                               \
+        type tmp = arr[i];                                                              \
+        arr[i] = arr[j];                                                                \
+        arr[j] = tmp;                                                                   \
+    }                                                                                   \
 }
 
 DEFINE_SHUFFLE_ARRAY(int, int)
-DEFINE_SHUFFLE_ARRAY(double, double)
-DEFINE_SHUFFLE_ARRAY(float, float)
-
-/* count-if macro */
-#define DEFINE_COUNT_IF(type, suffix)                               \
-static inline int count_##suffix##_if(const type arr[], size_t len, \
-    bool (*predicate)(const type* arr, const type)) {               \
-        int count = 0;                                              \
-        for (size_t i = 0; i < len; i++)                            \
-            if (predicate(arr, arr[i])) count++;                    \
-        return count;                                               \
-}
-
-DEFINE_COUNT_IF(int, int)
-DEFINE_COUNT_IF(double, double)
-DEFINE_COUNT_IF(float, float)
-DEFINE_COUNT_IF(char, char)
-
-/* EXECUTE_AFTER macro: stays in header */
-#define EXECUTE_AFTER(start_time, seconds, action) \
-do {                                               \
-    if (second() - start_time >= seconds) {        \
-        action;                                    \
-    }                                              \
-} while (0)
-
 #endif
