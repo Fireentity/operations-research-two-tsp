5289cd4680fbb4cacd04c01243dfb6e74b8f3aad
Biblink02 <alby.bot.24@gmail.com>
2025-12-08 15:58:00 +0100

    feat: better tsplib parser

--- tsp_algo_lib/src/parser/instance/tsp_parser_tsplib.c
+++ tsp_algo_lib/src/parser/instance/tsp_parser_tsplib.c
@@ -3,33 +3,43 @@
 #include "tsp_parser.h"
 #include <string.h>
 
-int parse_instance_tsplib(const char *path, Node **out_nodes, int *out_n) {
+TspParserStatus parse_instance_tsplib(const char *path, Node **out_nodes, int *out_n) {
     FILE *f = fopen(path, "r");
     if (!f) return PARSE_ERR_OPEN;
 
     char token[128];
     int n = 0;
+    int have_nodes_section = 0;
     Node *nodes = NULL;
-    int section_nodes = 0;
-    int err = PARSE_ERR_FORMAT;
+
+    TspParserStatus status = PARSE_ERR_FORMAT;
 
     while (read_next_token(f, token, sizeof(token)) == 0) {
 
-        if (!section_nodes) {
+        size_t len = strlen(token);
+        if (len && token[len - 1] == ':')
+            token[len - 1] = '\0';
+        if (!token[0])
+            continue;
+
+        if (!have_nodes_section) {
+
             if (strcmp(token, "DIMENSION") == 0) {
                 if (read_next_token(f, token, sizeof(token)) != 0) break;
-                if (strcmp(token, ":") == 0)
-                    if (read_next_token(f, token, sizeof(token)) != 0) break;
+                if (strcmp(token, ":") == 0 &&
+                    read_next_token(f, token, sizeof(token)) != 0) break;
 
-                if (parse_int(token, &n) != 0 || n <= 0) goto cleanup;
+                if (parse_int(token, &n) != 0 || n <= 0) goto fail;
             }
+
             else if (strcmp(token, "NODE_COORD_SECTION") == 0) {
-                if (n <= 0) goto cleanup;
+                if (n <= 0) goto fail;
                 nodes = malloc(n * sizeof(Node));
-                if (!nodes) { err = PARSE_ERR_MEMORY; goto cleanup; }
-                section_nodes = 1;
-                err = PARSE_OK;
+                if (!nodes) { status = PARSE_ERR_MEMORY; goto fail; }
+                have_nodes_section = 1;
+                status = PARSE_OK;
             }
+
             else if (strcmp(token, "EOF") == 0) break;
         }
 
@@ -37,20 +47,22 @@ int parse_instance_tsplib(const char *path, Node **out_nodes, int *out_n) {
             if (strcmp(token, "EOF") == 0) break;
 
             int id;
-            if (parse_int(token, &id) != 0) goto cleanup;
-            if (id < 1 || id > n) goto cleanup;
+            if (parse_int(token, &id) != 0 || id < 1 || id > n)
+                { status = PARSE_ERR_INV; goto fail; }
 
             if (read_next_token(f, token, sizeof(token)) != 0 ||
-                parse_double(token, &nodes[id - 1].x) != 0) goto cleanup;
+                parse_double(token, &nodes[id - 1].x) != 0)
+                { status = PARSE_ERR_INV; goto fail; }
 
             if (read_next_token(f, token, sizeof(token)) != 0 ||
-                parse_double(token, &nodes[id - 1].y) != 0) goto cleanup;
+                parse_double(token, &nodes[id - 1].y) != 0)
+                { status = PARSE_ERR_INV; goto fail; }
         }
     }
 
     fclose(f);
 
-    if (!nodes || !section_nodes) {
+    if (!nodes || !have_nodes_section) {
         free(nodes);
         return PARSE_ERR_FORMAT;
     }
@@ -59,8 +71,8 @@ int parse_instance_tsplib(const char *path, Node **out_nodes, int *out_n) {
     *out_n = n;
     return PARSE_OK;
 
-    cleanup:
-        fclose(f);
+fail:
+    fclose(f);
     free(nodes);
-    return err;
+    return status;
 }
